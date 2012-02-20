e36826d98909f8cfe29045342b24fd1e37ed86f7
Nick Korasidis <renelvon@gmail.com>
2012-02-21 00:39:13 +0200

    Removing stray comments

--- prob16/main.py
+++ prob16/main.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-# Open big-numbers' file
 b = 2; e = 1000
 n = sum(map(int, str(b**e)))
 print('The sum of digits of', b, 'raised to the', e, 'th power is:', n)
--- prob20/main.py
+++ prob20/main.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-# Open big-numbers' file
 i = 100
 from math import factorial
 n = sum(map(int, str(factorial(i))))
